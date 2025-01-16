9efc55a89dc4b8bde151e5043b0f0ba05942e831
Mariia-Ann <Myshyck@gmail.com>
2025-01-16 10:25:56 +0000

    Deploying to gh-pages from @ Mariia-Ann/goit-js-hw-11@45ac73a14385285b28176df7a879bdad688b8727 🚀

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-import{S as f,i as a}from"./assets/vendor-5ObWk2rO.js";(function(){const n=document.createElement("link").relList;if(n&&n.supports&&n.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))t(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const s of r.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&t(s)}).observe(document,{childList:!0,subtree:!0});function i(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function t(e){if(e.ep)return;e.ep=!0;const r=i(e);fetch(e.href,r)}})();function p(o){return fetch(`https://pixabay.com/api/?key=48242250-abd806f4b021d280ea207da17&q=${o}&image_type=photo&orientation=horizontal&safesearch=true`).then(t=>{if(!t.ok)throw new Error(t.statusText);return t.json()}).then(t=>t).catch(t=>{throw console.log(t),t})}const c=document.querySelector(".card-container"),g=document.querySelector(".search-form"),l=document.querySelector(".loader");g.addEventListener("submit",y);function y(o){o.preventDefault();const n=o.currentTarget,i=n.elements.query.value;c.innerHTML="",w(),p(i).then(b).catch(L).finally(()=>{n.reset(),S()})}function b(o){if(o.hits&&o.hits.length>0){const n=o.hits.map(({webformatURL:t,largeImageURL:e,likes:r,views:s,comments:u,downloads:d,tags:h})=>{const m=h.split(",").join(", ");return`<div class="card">
-      <a href="${e}">
-        <img src="${t}" alt="${m}" />
-      </a>
-    <div class="card-properties">
-        <ul class="card-list">
-            <li>Likes <span class="accent-item">${r}</span></li>
-            <li>Views <span class="accent-item">${s}</span></li>
-            <li>Comments <span class="accent-item">${u}</span></li>
-            <li>Downloads <span class="accent-item">${d}</span></li>
-        </ul>
-    </div>
-    </div>`}).join("");c.insertAdjacentHTML("beforeend",n),new f(".card-container a",{captionsData:"alt",captionDelay:250}).refresh()}else a.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight",titleColor:"white",messageColor:"white",timeout:5e3,backgroundColor:"#EF4040"})}function L(o){console.log(o),a.error({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight",titleColor:"white",messageColor:"white",timeout:5e3,backgroundColor:"#EF4040"})}function w(){l.style.display="block"}function S(){l.style.display="none"}
+import{S as p,i as g}from"./assets/vendor-5ObWk2rO.js";(function(){const n=document.createElement("link").relList;if(n&&n.supports&&n.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))r(t);new MutationObserver(t=>{for(const o of t)if(o.type==="childList")for(const i of o.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&r(i)}).observe(document,{childList:!0,subtree:!0});function s(t){const o={};return t.integrity&&(o.integrity=t.integrity),t.referrerPolicy&&(o.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?o.credentials="include":t.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function r(t){if(t.ep)return;t.ep=!0;const o=s(t);fetch(t.href,o)}})();function y(e,n){if(e.hits&&e.hits.length>0){const s=e.hits.map(({webformatURL:t,largeImageURL:o,likes:i,views:u,comments:d,downloads:f,tags:h})=>{const m=h.split(",").join(", ");return`<div class="card">
+              <a href="${o}">
+                <img src="${t}" alt="${m}" />
+              </a>
+            <div class="card-properties">
+                <ul class="card-list">
+                    <li>Likes <span class="accent-item">${i}</span></li>
+                    <li>Views <span class="accent-item">${u}</span></li>
+                    <li>Comments <span class="accent-item">${d}</span></li>
+                    <li>Downloads <span class="accent-item">${f}</span></li>
+                </ul>
+            </div>
+            </div>`}).join("");n.insertAdjacentHTML("beforeend",s),new p(".card-container a",{captionsData:"alt",captionDelay:250}).refresh()}else l("Sorry, there are no images matching your search query. Please try again!")}function L(e){e.style.display="block"}function b(e){e.style.display="none"}function l(e){g.error({message:e,position:"topRight",titleColor:"white",messageColor:"white",timeout:5e3,backgroundColor:"#EF4040"})}function w(e){return fetch(`https://pixabay.com/api/?key=48242250-abd806f4b021d280ea207da17&q=${e}&image_type=photo&orientation=horizontal&safesearch=true`).then(r=>{if(!r.ok)throw new Error(r.statusText);return r.json()}).then(r=>r).catch(r=>{throw console.log(r),r})}const a=document.querySelector(".card-container"),S=document.querySelector(".search-form"),c=document.querySelector(".loader");S.addEventListener("submit",E);function E(e){e.preventDefault();const n=e.currentTarget,s=n.elements.query.value;a.innerHTML="",L(c),w(s).then(r=>y(r,a)).catch($).finally(()=>{n.reset(),b(c)})}function $(e){console.log(e),l("Sorry, there was an error with the request. Please try again later.")}
 //# sourceMappingURL=index.js.map
