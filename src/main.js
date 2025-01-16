45ac73a14385285b28176df7a879bdad688b8727
Mariia_S <Myshyck@gmail.com>
2025-01-16 12:25:34 +0200

    change render-functions.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+import {
+  renderImagesCard,
+  showLoadingIndicator,
+  hideLoadingIndicator,
+  showErrorMessage,
+} from './js/render-functions.js';
 
 import { fetchImages } from './js/pixabay-api.js';
 
@@ -18,86 +20,20 @@ function handleSearch(event) {
 
   cardContainer.innerHTML = '';
 
-  showLoadingIndicator();
+  showLoadingIndicator(loading);
 
   fetchImages(query)
-    .then(renderImagesCard)
+    .then(data => renderImagesCard(data, cardContainer))
     .catch(onFetchError)
     .finally(() => {
       form.reset();
-      hideLoadingIndicator();
+      hideLoadingIndicator(loading);
     });
-}
-
-function renderImagesCard(data) {
-  if (data.hits && data.hits.length > 0) {
-    const markup = data.hits
-      .map(
-        ({
-          webformatURL,
-          largeImageURL,
-          likes,
-          views,
-          comments,
-          downloads,
-          tags,
-        }) => {
-          const tagsString = tags.split(',').join(', ');
-          return `<div class="card">
-      <a href="${largeImageURL}">
-        <img src="${webformatURL}" alt="${tagsString}" />
-      </a>
-    <div class="card-properties">
-        <ul class="card-list">
-            <li>Likes <span class="accent-item">${likes}</span></li>
-            <li>Views <span class="accent-item">${views}</span></li>
-            <li>Comments <span class="accent-item">${comments}</span></li>
-            <li>Downloads <span class="accent-item">${downloads}</span></li>
-        </ul>
-    </div>
-    </div>`;
-        }
-      )
-      .join('');
-
-    cardContainer.insertAdjacentHTML('beforeend', markup);
-
-    const lightbox = new SimpleLightbox('.card-container a', {
-      captionsData: 'alt',
-      captionDelay: 250,
-    });
-
-    lightbox.refresh();
-  } else {
-    iziToast.error({
-      message: `Sorry, there are no images matching your search query. Please try again!`,
-      position: 'topRight',
-      titleColor: 'white',
-      messageColor: 'white',
-      timeout: 5000,
-      backgroundColor: '#EF4040',
-    });
-  }
 }
 
 function onFetchError(error) {
   console.log(error);
-
-  iziToast.error({
-    message: `Sorry, there are no images matching your search query. Please try again!`,
-    position: 'topRight',
-    titleColor: 'white',
-    messageColor: 'white',
-    timeout: 5000,
-    backgroundColor: '#EF4040',
-  });
-}
-
-
-function showLoadingIndicator() {
-  loading.style.display = 'block';
-}
-
-function hideLoadingIndicator() {
-  loading.style.display = 'none';
+  showErrorMessage(
+    'Sorry, there was an error with the request. Please try again later.'
+  );
 }
